8743e50613c6aa2b0ca063f32a987972f2f25ac7
LlewVallis <llewvallis@gmail.com>
2020-10-22 20:03:56 +1100

    Disable caching for index.html

--- src/main/java/org/astropeci/urmwstats/WebApplicationConfiguration.java
+++ src/main/java/org/astropeci/urmwstats/WebApplicationConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +24,13 @@ public class WebApplicationConfiguration implements WebMvcConfigurer {
         return container -> {
             container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound"));
         };
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("classpath:/static/index.html")
+                .setCacheControl(CacheControl.noCache())
+                .setCachePeriod(0);
     }
 }
